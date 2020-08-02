614be951deabe34ec1f18f693eacd4ea374fc8d8
Stefanos Apkarian <stefanos.apkarian@gmail.com>
2020-08-02 12:03:04 +0300

    First push

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pizzas', function () {
+
+    $pizzas = [
+        ['type' => 'hawaiian', 'base' => 'cheese crust', 'price' => '10'],
+        ['type' => 'mexican', 'base' => 'garlic crust', 'price' => '15'],
+        ['type' => 'chicken', 'base' => 'mayo crust', 'price' => '20'],
+    ];
+    return view('pizzas', ['pizzas' => $pizzas]);
+});
