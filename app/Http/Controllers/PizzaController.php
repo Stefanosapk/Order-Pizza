ded0991204c6c27557f9e21b5dd6680bbe8e511b
Stefanos Apkarian <stefanos.apkarian@gmail.com>
2020-08-03 21:57:30 +0300

    Name conventions and better structure + new page(/pizzas/create)

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -13,15 +13,19 @@ class PizzaController extends Controller
 //        $pizzas = Pizza::where('type', 'hawaiian')->get();
 //        $pizzas = Pizza::latest()->get();
 
-        return view('pizzas', [
+        return view('pizzas.index', [
             'pizzas' => $pizzas,
             'name' => request('name'),
             'age' => request('age')
         ]);
     }
     public function show($id){
-        return view('details', [
+        return view('pizzas.show', [
             'id' => $id,
         ]);
+    }
+
+    public function create(){
+        return view('pizzas.create');
     }
 }
