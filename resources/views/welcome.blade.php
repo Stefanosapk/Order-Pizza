1be85ffa2e32116a10182c321eb40a72d8b5af09
Stefanos Apkarian <stefanos.apkarian@gmail.com>
2020-08-08 12:16:29 +0300

    message Registration are closed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}">Register</a>
+                    @else
+                        <span>Registrations are closed!</span>
                     @endif
                 @endauth
             </div>
